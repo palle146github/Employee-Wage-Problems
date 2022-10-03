1f75789330e67eff96480f86a16d77bdcc9480bb
palle146github <prasanthpalle146@gmail.com>
2022-10-03 18:59:22 +0530

    UC3

---
+++ UC3_EmployeWageFullTimePartTime.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmpWageProb
+{
+    class Program
+    {
+        public const int EMP_RATE_PER_HOUR = 20;
+        public const int IS_PART_TIME = 1;
+        public const int IS_FULL_TIME = 2;
+        public static void EmpWagePartFullTime(string[] args)
+        {
+            int empwage = 0;
+            int emphours = 0;
+            Random random = new Random();
+            int empcheck = random.Next(0, 3);
+
+            if (empcheck == IS_FULL_TIME)
+            {
+                emphours = 8;
+            }
+            else if (empcheck == IS_PART_TIME)
+            {
+                emphours = 4;
+            }
+            else
+            {
+                emphours = 0;
+            }
+            empwage = emphours * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Employee Wage " + empwage);
+            Console.ReadKey();
+        }
+    }
+}
